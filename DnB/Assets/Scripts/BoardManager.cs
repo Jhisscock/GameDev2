6e7ed693b673906b72e623367ad07ea1a48f083b
Jhisscock <jthisscock@mail.lipscomb.edu>
2019-12-08 13:06:19 -0600

    DnB

---
+++ DnB/Assets/Scripts/BoardManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+    private bool [,] board; 
+    public GameObject dot;
+    public GameObject line;
+    // Start is called befor.e the first frame update
+    void Start()
+    {
+        board = new bool[3, 2];
+        int vertCount = 0;
+        int horizCount = 0;
+        for(int i = 0; i < (board.GetLength(0) + 1) * board.GetLength(1); i++){
+            Instantiate(line, new Vector2((float)horizCount * 2 - 1f, (float)vertCount * 2 - 1f), Quaternion.Euler(0f, 0f, 90f));
+            if(horizCount >= board.GetLength(0)){
+                horizCount = 0;
+                vertCount++;
+            }else{
+                horizCount++;
+            }
+        }
+        vertCount = 0;
+        horizCount = 0;
+        for(int i = 0; i < board.GetLength(0) * board.GetLength(0); i++){
+            Instantiate(line, new Vector2((float)horizCount * 2, (float)vertCount*2 - 2), Quaternion.identity);
+            if(horizCount >= board.GetLength(0) - 1){
+                horizCount = 0;
+                vertCount++;
+            }else{
+                horizCount++;
+            }
+        }
+        vertCount = 0;
+        horizCount = 0;
+        for(int i = 0; i < (board.GetLength(0) + 1) * board.GetLength(0); i++){
+            Instantiate(dot, new Vector2((float)horizCount * 2 - 1f, (float)vertCount * 2 - 2f), Quaternion.identity);
+            if(horizCount >= board.GetLength(0)){
+                horizCount = 0;
+                vertCount++;
+            }else{
+                horizCount++;
+            }
+        }
+        //Camera.main.transform.position = new Vector3((board.GetLength(0) + 1) / 2, 0f, 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+     void AddLine(){
+
+    }
+}
---
+++ DnB/Assets/Scripts/LineDisplay.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineDisplay : MonoBehaviour
+{
+    private Color startColor;
+    private bool clicked = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startColor = GetComponent<SpriteRenderer>().color;
+        GetComponent<SpriteRenderer>().color = Color.clear;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseExit(){
+        if(!clicked){
+            GetComponent<SpriteRenderer>().color = Color.clear;
+        }else{
+            GetComponent<SpriteRenderer>().color = startColor;
+        }
+    }
+
+    void OnMouseOver(){
+        GetComponent<SpriteRenderer>().color = startColor;
+    }
+
+    void OnMouseDown(){
+        GetComponent<BoxCollider2D>().enabled = false;
+        clicked = true;
+    }
+}
