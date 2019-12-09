b95bbf37960d00f8c724221b9ff141bedfde438d
Jacob T Hisscock <37636270+Jhisscock@users.noreply.github.com>
2019-12-09 10:25:54 -0600

    turn-base

--- DnB/Assets/Scripts/BoardManager.cs
+++ DnB/Assets/Scripts/BoardManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class BoardManager : MonoBehaviour
 {
@@ -13,6 +14,9 @@ public class BoardManager : MonoBehaviour
     public GameObject p2Square;
     public static BoardManager Instance;
     public GameObject line2;
+    private int turnCounter = 0;
+    private int p1Score;
+    private int p2Score;
     
     void Awake(){
         Instance = this;
@@ -29,7 +33,7 @@ public class BoardManager : MonoBehaviour
             }
         }
         for(int i = 0; i < (board.GetLength(0) - 6) * (board.GetLength(1) - 7); i++){
-            Instantiate(line, new Vector2((float)horizCount * 2, (float)vertCount * 2 + 1), Quaternion.Euler(0f, 0f, 90f));
+            PhotonNetwork.InstantiateSceneObject("Line", new Vector2((float)horizCount * 2, (float)vertCount * 2 + 1), Quaternion.Euler(0f, 0f, 90f));
             if(horizCount >= board.GetLength(0) - 7){
                 horizCount = 0;
                 vertCount++;
@@ -40,7 +44,7 @@ public class BoardManager : MonoBehaviour
         vertCount = 0;
         horizCount = 0;
         for(int i = 0; i < (board.GetLength(0) - 7) * (board.GetLength(0) - 7); i++){
-            Instantiate(line, new Vector2((float)horizCount * 2 + 1, (float)vertCount*2), Quaternion.identity);
+            PhotonNetwork.InstantiateSceneObject("Line", new Vector2((float)horizCount * 2 + 1, (float)vertCount*2), Quaternion.identity);
             if(horizCount >= board.GetLength(0) - 8){
                 horizCount = 0;
                 vertCount++;
@@ -51,7 +55,7 @@ public class BoardManager : MonoBehaviour
         vertCount = 0;
         horizCount = 0;
         for(int i = 0; i < (board.GetLength(0) - 6) * (board.GetLength(0) - 7); i++){
-            Instantiate(dot, new Vector2((float)horizCount * 2 , (float)vertCount * 2 ), Quaternion.identity);
+            PhotonNetwork.InstantiateSceneObject("Dot", new Vector2((float)horizCount * 2 , (float)vertCount * 2 ), Quaternion.identity);
             if(horizCount >= board.GetLength(0) - 7){
                 horizCount = 0;
                 vertCount++;
@@ -76,16 +80,40 @@ public class BoardManager : MonoBehaviour
     void CheckBox(int x, int y){
         if(y % 2 == 0){
             if(board[x - 1,y + 1] && board[x + 1, y + 1] && board[x,y + 2]){
-                Instantiate(p1Sqaure, new Vector2((float)x - 2, (float)y - 1), Quaternion.identity);
+                if(PhotonNetwork.IsMasterClient){
+                    PhotonNetwork.InstantiateSceneObject("Player1", new Vector2((float)x - 2, (float)y - 1), Quaternion.identity);
+                    p1Score++;
+                }else{
+                    PhotonNetwork.InstantiateSceneObject("Player2", new Vector2((float)x - 2, (float)y - 1), Quaternion.identity);
+                    p2Score++;
+                }
             }else if(board[x + 1,y - 1] && board[x - 1, y - 1] && board[x,y - 2]){
-                Instantiate(p1Sqaure, new Vector2((float)x - 2, (float)y - 3), Quaternion.identity);
+                if(PhotonNetwork.IsMasterClient){
+                    PhotonNetwork.InstantiateSceneObject("Player1", new Vector2((float)x - 2, (float)y - 3), Quaternion.identity);
+                    p1Score++;
+                }else{
+                    PhotonNetwork.InstantiateSceneObject("Player2", new Vector2((float)x - 2, (float)y - 3), Quaternion.identity);
+                    p2Score++;
+                }
+            }
         }else{
             if(board[x + 1 ,y - 1] && board[x + 1, y + 1] && board[x + 2,y]){
-                Instantiate(p1Sqaure, new Vector2((float)x - 1, (float)y - 2), Quaternion.identity);
+                if(PhotonNetwork.IsMasterClient){
+                    PhotonNetwork.InstantiateSceneObject("Player1", new Vector2((float)x - 1, (float)y - 2), Quaternion.identity);
+                    p1Score++;
+                }else{
+                    PhotonNetwork.InstantiateSceneObject("Player2", new Vector2((float)x - 1, (float)y - 2), Quaternion.identity);
+                    p2Score++;
+                }
             }else if(board[x - 1,y + 1] && board[x - 1, y - 1] && board[x - 2,y]){
-                Instantiate(p1Sqaure, new Vector2((float)x - 3, (float)y - 2), Quaternion.identity);
+                if(PhotonNetwork.IsMasterClient){
+                    PhotonNetwork.InstantiateSceneObject("Player1", new Vector2((float)x - 3, (float)y - 2), Quaternion.identity);
+                    p1Score++;
+                }else{
+                    PhotonNetwork.InstantiateSceneObject("Player2", new Vector2((float)x - 3, (float)y - 2), Quaternion.identity);
+                    p2Score++;
+                }
             }
         }
     }
-}
 }
